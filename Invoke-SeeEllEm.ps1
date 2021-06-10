95a34999056176b11422150e121387dbdc932ccc
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-06-10 19:25:14 +0100

    Update Invoke-SeeEllEm.ps1

--- Invoke-SeeEllEm.ps1
+++ Invoke-SeeEllEm.ps1
@@ -1,30 +1,30 @@
 function Create-AppIl
 {
 <#
-	.SYNOPSIS
-		Generates a dll with an arbitary Entrypoint and powershell commands. 
-		Author: p4yl0ad (p4yl0ad@protonmail.com)  
-		License: BSD 3-Clause  
-		Required Dependencies: System.Management.Automation.dll in current working directory
+.SYNOPSIS
+	Generates a dll with an arbitary Entrypoint and powershell commands. 
+	Author: p4yl0ad (p4yl0ad@protonmail.com)  
+	License: BSD 3-Clause  
+	Required Dependencies: System.Management.Automation.dll in current working directory
 
-	.DESCRIPTION
-		Utilizes System.Management.Automation.Runspaces in order to create a powershell runspace
-		
-	.PARAMETER DllName
-		Specifies the Name for the dll, if ommited iso standard date will be used for the binary
+.DESCRIPTION
+	Utilizes System.Management.Automation.Runspaces in order to create a powershell runspace
+	
+.PARAMETER DllName
+	Specifies the Name for the dll, if ommited iso standard date will be used for the binary
 
-	.PARAMETER Entry
-		Specifies the Entry point for the dll
+.PARAMETER Entry
+	Specifies the Entry point for the dll
 
-	.PARAMETER Command
-		The command to be executed in the powershell runspace
+.PARAMETER Command
+	The command to be executed in the powershell runspace
 
-	.PARAMETER Build
-		Build the il to a dll with a patched entrypont
+.PARAMETER Build
+	Build the il to a dll with a patched entrypont
 
-	.EXAMPLE
-		Create-AppIl -DllName test1 -Entry test2 -Command test4 -Build
-		Create-AppIl -DllName "ayylmao" -Entry "Poon" -Command "$ExecutionContext.SessionState.LanguageMode > C:\Windows\Tasks\bypass.txt" -Build	
+.EXAMPLE
+	Create-AppIl -DllName test1 -Entry test2 -Command test4 -Build
+	Create-AppIl -DllName "ayylmao" -Entry "Poon" -Command "$ExecutionContext.SessionState.LanguageMode > C:\Windows\Tasks\bypass.txt" -Build	
 #>
 param
 (
